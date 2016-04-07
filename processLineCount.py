73b717666724d6163674d26f57e717b72106beaa
ZackC <zack_coker@yahoo.com>
2016-04-07 14:06:28 -0400

    committing processLineCount.py to continue editing on another machine

--- processLineCount.py
+++ processLineCount.py
@@ -5,6 +5,8 @@
 import sys
 import re
 import os
+import subprocess
+import traceback
 
 minChange=0.50
 
@@ -15,46 +17,76 @@ def main(argv=None):
   else:
     fin = open(sys.argv[1],'r')
     foundCount = False
+    commitName="not set"
+    previousCommitName = "not set"
+    commitIsCorrect=False
+    containsDigitPattern = re.compile(".*\d+.*")
+    containsLetterPattern = re.compile(".*[A-Za-z]+.*")
     for line in fin:
-      if line.startswith("SUM:"):
-        lineItems = line.split()
-        #print "items in line"
-        count=0
-        oldCommentCount = int(lineItems[3])
-        oldSourceLines = int(lineItems[4])
-        if foundCount:
-	  '''if sourceLines != 0 and oldSourceLines != 0:
-            oldRatio = float(oldCommentCount)/float(oldSourceLines)
-            newRatio = float(commentCount)/float(sourceLines)
-            relativeChange = abs(newRatio-oldRatio)
-            if relativeChange > minChange:
-              print "change in: "+os.getcwd()+"/"+sys.argv[1]
-              print "old comment count: "+str(oldCommentCount)+", old source count: "+str(oldSourceLines)+", old ratio: "+str(oldRatio)
-              print "new comment count: "+str(commentCount)+", new source count: "+str(sourceLines)+", new ratio: "+str(newRatio)
-              print "total change: "+str(relativeChange)
-              print "============================"
-            else:
-              print "total change: "+str(relativeChange)
-              print "============================"'''
-          if oldSourceLines != 0 and oldCommentCount != 0:
-            commentRatio = float(commentCount)/float(oldCommentCount)
-            sourceRatio = float(sourceLines)/float(oldSourceLines)
-            #relativeChange = abs(commentRatio-sourceRatio)
-            relativeChange = commentRatio - sourceRatio
-            if relativeChange > minChange and commentCount - oldCommentCount > 50:
-              print "change in: "+os.getcwd()+"/"+sys.argv[1]
-              print "old comment count: "+str(oldCommentCount)+", new comment count: "+str(commentCount)+", old ratio: "+str(commentRatio)
-              print "old source count: "+str(oldSourceLines)+", new source count: "+str(sourceLines)+", new ratio: "+str(sourceRatio)
-              print "total change: "+str(relativeChange)
-              print "============================"
+      try:
+        if line.startswith("SUM:") and commitIsCorrect:
+          lineItems = line.split()
+          #print "items in line"
+          count=0
+          oldCommentCount = int(lineItems[3])
+          oldSourceLines = int(lineItems[4])
+          if foundCount:
+            '''if sourceLines != 0 and oldSourceLines != 0:
+              oldRatio = float(oldCommentCount)/float(oldSourceLines)
+              newRatio = float(commentCount)/float(sourceLines)
+              relativeChange = abs(newRatio-oldRatio)
+              if relativeChange > minChange:
+                print "change in: "+os.getcwd()+"/"+sys.argv[1]
+                print "old comment count: "+str(oldCommentCount)+", old source count: "+str(oldSourceLines)+",   old ratio: "+str(oldRatio)
+                print "new comment count: "+str(commentCount)+", new source count: "+str(sourceLines)+", new ratio: "+str(newRatio)
+                print "total change: "+str(relativeChange)
+                print "============================"
+              else:
+                print "total change: "+str(relativeChange)
+                print "============================"'''
+            if oldSourceLines != 0 and oldCommentCount != 0:
+              commentRatio = float(commentCount)/float(oldCommentCount)
+              sourceRatio = float(sourceLines)/float(oldSourceLines)
+              #relativeChange = abs(commentRatio-sourceRatio)
+              relativeChange = commentRatio - sourceRatio
+              diffResult=subprocess.check_output(["git","diff",previousCommitName,commitName])
+              fout=open("gitDiffResult.txt",'w')
+              fout.write(diffResult)
+              fout.close()
+              licenseLineResult=subprocess.check_output(["python","../../countLicenseLines.py","gitDiffResult.txt"])
+              licenseLineResult=licenseLineResult.rstrip()
+              colonIndex=licenseLineResult.find(":")
+              licenseLineCount=licenseLineResult[colonIndex+1:len(licenseLineResult)]
+              licenseLineCount=int(licenseLineCount)
+            
+              if relativeChange > minChange and commentCount - oldCommentCount - licenseLineCount > 50:
+                print "change in: "+os.getcwd()+"/"+sys.argv[1]
+                print "new commit: %s" % (commitName)
+                print "old commit: %s" % (previousCommitName)
+                print "old comment count: "+str(oldCommentCount)+", new comment count: "+str(commentCount)+", old ratio: "+str(commentRatio)
+                print "old source count: "+str(oldSourceLines)+", new source count: "+str(sourceLines)+", new ratio: "+str(sourceRatio)
+                print "total change: "+str(relativeChange)
+                print "============================"
             #else:
               #print "total change: "+str(relativeChange)
               #print "============================"
+          else:
+            foundCount = True
+          commentCount = oldCommentCount
+          sourceLines = oldSourceLines
         else:
-          foundCount = True
-        commentCount = oldCommentCount
-        sourceLines = oldSourceLines
-        
+          if line.startswith("current commit: "):
+            lineItems = line.split()
+            if lineItems[2].isalnum() and re.match(containsDigitPattern, lineItems[2]) != None and re.match(containsLetterPattern, lineItems[2]) != None:
+              commitName = previousCommitName
+              previousCommitName = lineItems[2]
+              commitIsCorrect=True
+            else:
+              commitIsCorrect=False
+      except:
+        traceback.print_exc()
+        print "error occured in the script!!"
+        #sys.exit(1)
 
 
 if __name__ == "__main__":
